d907c06a6065e288f209e6de139280abbff3e1b3
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-24 17:43:46 -0600

    Created a custom button

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -72,6 +72,8 @@ class _ButtonsView extends StatelessWidget {
               label: const Text('Text Icon'),
             ),
             
+            const _CustomButton(),
+
             IconButton(onPressed: (){}, icon: const Icon( Icons.app_registration_rounded)),
             IconButton(
               onPressed: (){}, 
@@ -82,6 +84,30 @@ class _ButtonsView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomButton extends StatelessWidget {
+  const _CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20 ,vertical: 10),
+              child: Text("Hello custom button", style: TextStyle(color: Colors.white),)
+            ),
         ),
       ),
     );
