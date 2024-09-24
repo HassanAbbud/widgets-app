5a8dd10a12b88abb906a7634d02d90a31dc60d05
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-24 12:27:13 -0600

    Styles and themes

---
+++ lib/config/themes/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.orange,
+  Colors.red,
+  Colors.blue,
+  Colors.purple,
+  Colors.teal,
+  Colors.pink,
+  Colors.yellow,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0}) 
+  : assert(selectedColor >= 0, "Selected must be greater than 0"),
+  assert(selectedColor <= colorList.length, "Selected must less or equal than ${colorList.length - 1}");
+
+
+  ThemeData getTheme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColor], 
+    );
+  }
+}
