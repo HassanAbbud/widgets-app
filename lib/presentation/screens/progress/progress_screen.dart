16284d6f4a7c513b7ea451a71940f70c374716e4
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-25 11:26:34 -0600

    Controlled progressive indicators: Linear and Circular

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -47,8 +47,32 @@ class _ProgressView extends StatelessWidget {
 class _ControlledProgressIndicator extends StatelessWidget {
   const _ControlledProgressIndicator();
 
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: Stream.periodic( const Duration( milliseconds: 300 ), (value) {
+        return (value * 2) / 100; // 0.0, 1.0
+      }).takeWhile((value) => value < 100 ),
+      builder: (context, snapshot) {
+
+        final progressValue = snapshot.data ?? 0;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator( value: progressValue, strokeWidth: 2, backgroundColor: Colors.black12, ),
+              const SizedBox(width: 20,),
+              Expanded(
+                child: LinearProgressIndicator(value: progressValue )
+              ),
+              
+            ],
+          ),
+        );
+
+      },
+    );
   }
 }
