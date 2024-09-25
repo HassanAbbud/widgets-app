fe48277f7502e3dc7378456d06cddcf945208bd8
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-25 16:31:06 -0600

    Added images and put PageView within Stack

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SlideInfo {
   final String title;
@@ -25,15 +26,30 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: slides.map(
-          (slideData) => _Slide(
-            title: slideData.title,
-            caption: slideData.caption,
-            imageUrl: slideData.imageUrl
-          )
-        ).toList(),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          
+          PageView(
+            physics: const BouncingScrollPhysics(),
+            children: slides.map(
+              (slideData) => _Slide(
+                title: slideData.title,
+                caption: slideData.caption,
+                imageUrl: slideData.imageUrl
+              )
+            ).toList(),
+          ),
+          
+      Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              child: const Text('Exit'),
+              onPressed: () => context.pop(),
+            )
+          ),
+        ],
       ),
     );
   }
@@ -49,6 +65,26 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            const SizedBox(height: 20,),
+            Text(title, style: titleStyle,),
+            const SizedBox(height: 20,),
+            Text(caption, style: captionStyle,),
+
+          ],
+        ),
+      ),
+    );
   }
 }
