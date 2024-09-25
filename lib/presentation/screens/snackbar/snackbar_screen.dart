44134cf51156e9f907e62b73a5d7173434d44a88
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-25 12:14:08 -0600

    Dialogues implemented

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   
@@ -17,6 +18,24 @@ class SnackbarScreen extends StatelessWidget {
 
     ScaffoldMessenger.of(context).showSnackBar(snakback);
   }
+  
+  void openDialog( BuildContext context ) {
+
+    showDialog(
+      context: context, 
+      barrierDismissible: false, //Force to close with buttons
+      builder: (context) => AlertDialog(
+        title: const Text('¿Are you sure?'),
+        content: const Text('Ex sit ea est in. Aliqua cillum amet tempor id officia tempor adipisicing amet commodo aliqua. Ut eiusmod cillum nulla sit ut non minim laboris cupidatat exercitation minim magna pariatur est. Nisi aute ea id enim sint occaecat aute adipisicing duis qui nisi. Nulla cillum nulla eiusmod enim magna sint eu dolor occaecat cupidatat.'),
+        actions: [
+          TextButton( onPressed: ()=> context.pop(), child: const Text('Cancel')),
+          
+          FilledButton( onPressed: ()=> context.pop(), child: const Text('Confirm')),
+        ],
+      ),
+    );
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +43,29 @@ class SnackbarScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Snackbars and dialogues"),
       ),
+
+       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            FilledButton.tonal(
+              onPressed: (){
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('Aute amet dolore ipsum ex adipisicing incididunt pariatur eiusmod ipsum duis voluptate commodo qui. Non anim nulla enim do sunt minim anim sit nostrud sunt. Ex culpa ipsum voluptate ut qui Lorem ad. Consequat dolor duis nulla ipsum ea. Nostrud nulla nulla ad pariatur ullamco cupidatat aliquip irure dolore amet deserunt nulla veniam. Id ut duis nulla in commodo elit. Laboris ea ipsum aute excepteur quis magna elit est incididunt veniam deserunt ad veniam.')
+                  ]
+                );
+              }, child: const Text('Licenses')),
+
+            FilledButton.tonal(
+              onPressed: () => openDialog(context), 
+              child: const Text('Show dialogue'))
+          ],
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Show Snackbar'),
         icon: const Icon( Icons.remove_red_eye_outlined ),
