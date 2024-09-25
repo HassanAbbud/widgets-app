a59ab4db1453b7171206733f2325f6f577f74aad
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-25 11:12:16 -0600

    Progress screen: Progress indicator added. Also stless. Widget for future _ControlledProgressIndicator

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -8,6 +8,44 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Progress indicators"),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Text('Circular progress indicator'),
+          SizedBox(height: 10),
+          CircularProgressIndicator( strokeWidth: 2, backgroundColor: Colors.black45 ),
+          
+          SizedBox(height: 20),
+          Text('Circular and linear controlled'),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator(),
+
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
