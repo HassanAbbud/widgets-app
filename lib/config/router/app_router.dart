ca9596ae2cc8d9472c3023a2a4bf43e20282ea15
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-25 13:05:42 -0600

    Simplified imports and added export for SnackbarScreen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,10 +1,6 @@
 import 'package:go_router/go_router.dart';
-import 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/home/home_screen.dart';
-import 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
-import 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+
+import 'package:widgets_app/presentation/screens/screens.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
