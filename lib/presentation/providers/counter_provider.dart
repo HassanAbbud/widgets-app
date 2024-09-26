e1af0f8c8557429996aaeb19096ab69189c35fd8
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-26 12:17:56 -0600

    Change light/dark mode button with provider setup

--- lib/presentation/providers/counter_provider.dart
+++ lib/presentation/providers/counter_provider.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterProvider = StateProvider((ref) => 5,);
+final counterProvider = StateProvider<int>((ref) => 5,);
+final darkModeProvider = StateProvider<bool>((ref) => false,);
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -11,9 +11,17 @@ class CounterScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkModeActive = ref.watch(darkModeProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Counter screen"),
+      actions: [
+        IconButton(
+          onPressed: () => ref.read(darkModeProvider.notifier).state = !isDarkModeActive, 
+          icon: isDarkModeActive ? const Icon(Icons.light_mode_outlined) : const Icon(Icons.dark_mode_outlined),  
+        )
+      ],
       ),
 
       body: Center(
