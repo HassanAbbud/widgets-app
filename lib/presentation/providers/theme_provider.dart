e71432f220fc8807b9c2e038c83d9b33985d2f6d
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-26 13:09:53 -0600

    Refactor: comments

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/themes/app_theme.dart';
 import 'package:widgets_app/presentation/screens/screens.dart';
 
+// Mutable dark mode state
 final darkModeProvider = StateProvider<bool>((ref) => false,);
 
+// Immutable color list
 final colorListProvider = Provider<List<Color>>((ref) => colorList,);
