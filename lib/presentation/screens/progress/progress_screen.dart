94e39a95b203c43401d360f6a9f22cd7e411b8c4
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-26 17:06:36 -0600

    Changed background color of CircularProgressIndicator

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -23,20 +23,20 @@ class _ProgressView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final colors = Theme.of(context).colorScheme;
+    // final colors = Theme.of(context).colorScheme;
 
-    return Center(
+    return const Center(
       child: Column(
         children: [
-          const SizedBox(height: 30),
-          const Text('Circular progress indicator'),
-          const SizedBox(height: 10),
-          CircularProgressIndicator( strokeWidth: 2, backgroundColor: colors.primary ),
+          SizedBox(height: 30),
+          Text('Circular progress indicator'),
+          SizedBox(height: 10),
+          CircularProgressIndicator( strokeWidth: 2, backgroundColor: Colors.black12 ),
           
-          const SizedBox(height: 20),
-          const Text('Circular and linear controlled'),
-          const SizedBox(height: 10),
-          const _ControlledProgressIndicator(),
+          SizedBox(height: 20),
+          Text('Circular and linear controlled'),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator(),
 
         ],
       ),
