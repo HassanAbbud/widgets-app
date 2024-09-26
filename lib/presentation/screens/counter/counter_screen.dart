8f907f81009f6791f3574932adfdfb67b3dea783
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-26 11:50:00 -0600

    Added provider riverpod, changed statless widget to ConsumerWidget & added reference to provider in CounterScreen

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
 
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 
+    final int clickCounter = ref.watch(counterProvider);
     int myCounter = 0;
 
     return Scaffold(
@@ -16,10 +19,10 @@ class CounterScreen extends StatelessWidget {
       ),
 
       body: Center(
-        child: Text("Value $myCounter", style: Theme.of(context).textTheme.titleLarge), ),
+        child: Text("Value: $clickCounter", style: Theme.of(context).textTheme.titleLarge), ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () => myCounter++,
+        onPressed: () => myCounter++, //change to clickCounter
         child: const Icon(Icons.add)
       )
     );
